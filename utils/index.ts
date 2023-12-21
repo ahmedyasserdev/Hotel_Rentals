44a3cd2e98782370efa547d90f12a0179981d26e
ahmedyasser <ahmedyasser3033@gmail.com>
2023-12-21 23:23:47 +0200

    updated

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,7 @@
-export const options = {
+export const options: RequestInit = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY  ,
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY  ?? '',
     "X-RapidAPI-Host": "bayut.p.rapidapi.com",
   },
 };
-
