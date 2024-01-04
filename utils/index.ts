0b26ddf3c4baa341a9600e8828016785a8e45db9
ahmedyasser <ahmedyasser3033@gmail.com>
2024-01-04 11:56:27 +0200

    updated

--- utils/index.ts
+++ utils/index.ts
@@ -5,6 +5,7 @@ export const options: RequestInit = {
   headers: {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY!,
     "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+    Origin: "https://luxury-rentals.vercel.app/",
   },
 };
 
