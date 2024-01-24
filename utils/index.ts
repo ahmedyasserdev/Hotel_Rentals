a08fab00783bc90767fcf4a3206598961857d516
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-01-25 00:04:59 +0200

    updated the api key

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { Data, DataProps } from "@/types";
 export const options: RequestInit = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.API_KEY!,
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_API_KEY  || '',
     "X-RapidAPI-Host": "bayut.p.rapidapi.com",
   },
 };
