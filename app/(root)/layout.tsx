a5895f42d707c4381d35de864c9ca8ef29bf81a9
Ahmed Yasser <134316723+ahmedyasserdev@users.noreply.github.com>
2024-01-31 21:01:55 +0200

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import '../globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import { ClerkProvider } from '@clerk/nextjs'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+  
 
     <html lang="en">
       <body >
@@ -30,7 +30,7 @@ export default function RootLayout({
           </main>
       </body>
     </html>
-    </ClerkProvider>
+  
 
   )
 }
